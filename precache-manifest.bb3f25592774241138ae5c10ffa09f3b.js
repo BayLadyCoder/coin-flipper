b0263ff70351270b5ad8bb65e72ba501dd6883e5
BayLadyCoder <rachada_32@hotmail.com>
2019-08-10 20:27:12 -0400

    Updates

---
+++ precache-manifest.bb3f25592774241138ae5c10ffa09f3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d0a2434f3205ca5443fc88825601c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e863985692ccff7a914d",
+    "url": "/static/css/main.f6707a9b.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "e863985692ccff7a914d",
+    "url": "/static/js/main.2582a1b3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
